d1b8c5995f129bf1bed1d24976337ad44e54ea2c
Desmond Yentumi <yentumid7@gmail.com>
2025-05-18 14:20:40 +0000

    done

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const username = document.getElementById("username");
 const email = document.getElementById("email")
 const password = document.getElementById("password")
-const confirm_password = document.getElementById("comfirm_password")
+const confirm_password = document.getElementById("confirm_password")
 
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", function(event) {
   event.preventDefault(); 
+  
+  const user = username.value;
+  const address = email.value;
+  const pass = password.value;
+  const passCon = confirm_password.value;
 
-  if (!username || !email || !password || !confirm_password) {
+  if (!user || !address || !pass || !passCon) {
     alert("Please fill in all fields.");
     return;
   }
 
-  if (password !== confirm_password) {
+  if (pass !== passCon) {
     alert("Passwords do not match.");
     return;
   }
@@ -22,3 +27,5 @@ form.addEventListener("submit", function (event) {
   alert("Form successfully submitted!");
   form.reset();
 });
+
+
